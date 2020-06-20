b133d13119528b378997b6a4a84889f255ae4a2b
brtsnhmls <67020090+brtsnhmls@users.noreply.github.com>
2020-06-20 21:39:27 +0200

    Update FirstClass.java
    dorduncu degisiklik

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,7 +6,7 @@ public class FirstClass {
 	System.out.println("first commit from local repository");
 	System.out.println("second commit from local repository");
 	System.out.println("conflict yapmak istiyorum");
-	System.out.println("quarte commit from remote repository");
+	System.out.println("quarte sorunlu repository");
 	
 		
 	}
